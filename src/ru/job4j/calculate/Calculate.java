b3d77011deaeb363a097a96cce3208abdbc9d588
Nikolay Govoruchin <govoruchin_nv@mail.ru>
2018-10-03 01:40:36 +0700

    trainee_001.1(JVM).1(Calculate: HelloWorld)

---
+++ src/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculate;
+/**
+ * Класс для вычисления арифметических операций
+ * @author Николай Говорухин (govoruchin_nv@mail.ru)
+ */
+public class Calculate {
+	/**
+	 * Method Main
+	 * @param args - args
+	 * @return void
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/ru/job4j/calculate/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package for calculate task.
+ * @author Николай Говорухин (govoruchin_nv@mail.ru)
+ */
+package ru.job4j.calculate;
