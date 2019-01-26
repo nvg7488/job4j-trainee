07ef7a7a5ee86a78db05f025f8db5ca5ac000463
nvg7488 <govoruchin_nv@mail.ru>
2019-01-26 13:47:30 +0700

    trainee_002.5(Исключения).4(Внутренний статический ItemShowAll)

--- chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
@@ -15,13 +15,12 @@ public class MenuTracker {
     }
     public int getActionsLentgh() { return  this.actions.size(); }
     public void fillActions() {
-//        this.actions.add(new ItemAdd(0, "Add Item."));
         this.actions.add(this.new ItemAdd(0, "Add Item."));
         this.actions.add(new ItemUpdate(1, "Update Item."));
         this.actions.add(new ItemDelete(2, "Delete Item."));
         this.actions.add(new ItemFindById(3, "Find Item by ID."));
         this.actions.add(new ItemFindByName(4, "Find Item by Name."));
-        this.actions.add(new ItemShowAll(5, "Show all Item."));
+        this.actions.add(new MenuTracker.ItemShowAll(5, "Show all Item."));
     }
     public void select(int key) { this.actions.get(key).execute(this.input, this.tracker); }
     public void show() {
@@ -49,6 +48,25 @@ public class MenuTracker {
             System.out.println("\t\tNew Item with ID: " + item.getId());
             System.out.println("\t\tNew Item with Name: " + item.getName());
             System.out.println("\t\tNew Item with Description: " + item.getDescription());
+        }
+        public String info() { return String.format("%d, %s", this.index, this.name); }
+    }
+    private static class ItemShowAll implements UserAction {
+        public int index;
+        public String name;
+        public ItemShowAll(int index, String name) {
+            this.index = index;
+            this.name = name;
+        }
+        public int key() { return this.index; }
+        public void execute(Input input, Tracker tracker) {
+            if (tracker.getPosition() == 0) {
+                System.out.println("Tracker пустой!");
+            } else {
+                for (Item item : tracker.findAll()) {
+                    item.printInfo();
+                }
+            }
         }
         public String info() { return String.format("%d, %s", this.index, this.name); }
     }
