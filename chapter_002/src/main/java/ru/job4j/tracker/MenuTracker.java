1a4707dc5f6dfe318e550a9c5fec2c42f28117c3
nvg7488 <govoruchin_nv@mail.ru>
2019-01-26 13:38:37 +0700

    trainee_002.5(Исключения).4(Внешний ItemAdd)

--- chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
@@ -15,7 +15,8 @@ public class MenuTracker {
     }
     public int getActionsLentgh() { return  this.actions.size(); }
     public void fillActions() {
-        this.actions.add(new ItemAdd(0, "Add Item."));
+//        this.actions.add(new ItemAdd(0, "Add Item."));
+        this.actions.add(this.new ItemAdd(0, "Add Item."));
         this.actions.add(new ItemUpdate(1, "Update Item."));
         this.actions.add(new ItemDelete(2, "Delete Item."));
         this.actions.add(new ItemFindById(3, "Find Item by ID."));
@@ -29,5 +30,26 @@ public class MenuTracker {
                 System.out.println(action.info());
             }
         }
+    }
+    private class ItemAdd implements UserAction {
+        public int index;
+        public String name;
+        public ItemAdd(int index, String name) {
+            this.index = index;
+            this.name = name;
+        }
+        public int key() { return this.index; }
+        public void execute(Input input, Tracker tracker) {
+            System.out.println("------------ Добавление новой заявки --------------");
+            String name = input.ask("Name: ");
+            String desc = input.ask("Desc: ");
+            Item item = new Item(name, desc, System.currentTimeMillis());
+            System.out.println("------------ Новая заявка с Id: " + item.getId() + "-----------");
+            tracker.addItem(item);
+            System.out.println("\t\tNew Item with ID: " + item.getId());
+            System.out.println("\t\tNew Item with Name: " + item.getName());
+            System.out.println("\t\tNew Item with Description: " + item.getDescription());
+        }
+        public String info() { return String.format("%d, %s", this.index, this.name); }
     }
 }
