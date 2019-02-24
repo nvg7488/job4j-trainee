8b347a0bd0207eed3c6b9b4c05f983a4a98144d7
nvg7488 <govoruchin_nv@mail.ru>
2019-02-24 15:48:44 +0700

    trainee_003.2(Обобщения).3(new tracker).1(addItem)

---
+++ chapter_003/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,104 @@
+package ru.job4j.tracker;
+/**
+ * Класс Tracker - обёртка над массивом.
+ * @author Николай Говорухин (govoruchin_nv@mail.ru)
+ */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+class Tracker {
+    /** Массив для хранения заявок. */
+    private List<Item> items;
+    /** Константа для генерации уникального ID. */
+    private static final Random RND = new Random();
+
+    Tracker() {
+        this.items = new ArrayList<>();
+    }
+    /**
+     * Добавить элемент в хранилище.
+     * @param item - добавляемый элемент
+     */
+    public Item addItem(Item item) {
+        item.setId(this.generateId());
+        items.add(item);
+        return item;
+    }
+    /**
+     * Генерирует ID.
+     * @return ID
+     */
+    private String generateId() {
+        return String.valueOf(System.currentTimeMillis() + RND.nextInt());
+    }
+    /**
+     * Заменить элемент в хранилище.
+     * @param item - то, чем заменяем элемент с таким же ID
+     */
+//    public void update(Item item) {
+//        String idToUpdate = item.getId();
+//        for (int index = 0; index < position; index++) {
+//            if (this.items[index].getId().equals(idToUpdate)) {
+//                this.items[index] = item;
+//                break;
+//            }
+//        }
+//    }
+    /**
+     * Удалить элемент в хранилище.
+     * @param id - ID удаляемой записи
+     */
+//    public void delete(String id) {
+//        for (int index = 0; index < position; index++) {
+//            if (this.items[index].getId().equals(id)) {
+//                this.items[index] = this.items[position - 1];
+//                this.items[position - 1] = null;
+//                position--;
+//                break;
+//            }
+//        }
+//    }
+    /**
+     * Вернуть массив из непустых элементов в хранилище.
+     * @return - возвращает массив Item
+     */
+    public List<Item> findAll() {
+        return new ArrayList<>(items);
+    }
+    /**
+     * Найти элемент по ID.
+     * @param id - указываем ID
+     * @return - возвращаемый элемент
+     */
+//    public Item findById(String id) {
+//        Item result = null;
+//        for (int index = 0; index < this.position; index++) {
+//            if (items[index].getId().equals(id)) {
+//                result = this.items[index];
+//                break;
+//            }
+//        }
+//        return result;
+//    }
+    /**
+     * Найти элемент по полю "name".
+     * @param key - текст для поиска
+     * @return - возвращает нужный элемент.
+     */
+//    public Item[] findByName(String key) {
+//        Item[] result = new Item[this.position];
+//        int newPosition = 0;
+//        for (int index = 0; index < this.position; index++) {
+//            if (items[index].getName().equals(key)) {
+//                result[newPosition] = items[index];
+//                newPosition++;
+//            }
+//        }
+//        result = Arrays.copyOf(result, newPosition);
+//        return result;
+//    }
+    /** Геттер указатель ячейки */
+//    public int getPosition() {
+//        return position;
+//    }
+}
