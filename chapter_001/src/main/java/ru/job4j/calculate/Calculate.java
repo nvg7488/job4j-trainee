7bebd38849d468cf8b5d3e22ef91126f03359cf5
nvg7488 <govoruchin_nv@mail.ru>
2019-03-02 21:54:30 +0700

    trainee_003.4(Инструменты).2(Travis CI).chapter_001.calculate

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculate;
+/**
+ * Класс для вычисления арифметических операций
+ * @author Николай Говорухин (govoruchin_nv@mail.ru)
+ */
+public class Calculate {
+	/**
+	 * Method Main
+	 * @param args - args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	public String echo(String name) {
+	    return "Echo, echo, echo : " + name;
+	}
+}
