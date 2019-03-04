0b160f4aecf06800cab13cf76dfa8f2c109da3b1
nvg7488 <govoruchin_nv@mail.ru>
2019-03-04 19:11:43 +0700

    trainee_003.4(Инструменты).2(Travis CI).chapter_002.Item,Tracker,TrackerTest

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,47 @@
+package ru.job4j.tracker;
+/**
+ * Класс Item.
+ * @author Николай Говорухин (govoruchin_nv@mail.ru)
+ */
+public class Item {
+    /** Уникальный id заявки */
+    private String id;
+    /** Не уникальное наименование заявки */
+    private String name;
+    /** Не уникальное описание заявки */
+    private String description;
+    /** Время добавления заявки */
+    private long create;
+
+    /**
+     * Конструктор Item.
+     * @param name        наименование заявки
+     * @param description описание заявки
+     * @param create      время добавления заявки
+     */
+    Item(String name, String description, long create) {
+        this.name = name;
+        this.create = create;
+        this.description = description;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getId() {
+        return this.id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public long getCreate() {
+        return this.create;
+    }
+
+    public void printInfo() {
+        System.out.println(String.format("Item ID: %s\nName: %s\nDescription: %s", this.getId(), this.getName(), this.getDescription()));
+    }
+}
