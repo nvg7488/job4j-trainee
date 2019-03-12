4036f7ab396a5ff85bdae4f0bc28e5eb85dcf3f6
nvg7488 <govoruchin_nv@mail.ru>
2019-03-12 18:13:40 +0700

    trainee_003.5(Контрольные вопросы).2(Банковские переводы).Remittance(заготовка)

---
+++ chapter_003/src/main/java/ru/job4j/remittance/Remittance.java
@@ -0,0 +1,35 @@
+package ru.job4j.remittance;
+/**
+ * @author Николай Говорухин (govoruchin_nv@mail.ru)
+ */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Remittance {
+    private Map<User, List<Account>> expenses = new HashMap<>();
+
+    /**   Добавление нового пользователя.   */
+    public void addUser(User user) {}
+    /**   Удаление пользователя.   */
+    public void deleteUser(User user) {}
+    /**   Добавление счёта зарегистрированному пользователю.   */
+    public void addAccountToUser(String passport, Account account) {}
+    /**   Уладение счёта у зарегистрированного пользователя.   */
+    public void deleteAccountFromUser(String passport, Account account) {}
+    /**   Получяет список счетов для пользователя.   */
+    public List<Account> getUserAccounts (String passport) {
+        List<Account> accounts = new ArrayList<>();
+        return accounts;
+    }
+    /**
+     * Метод для перечисления денег с одного счёта на другой счёт:
+     * если счёт не найден
+     * или не хватает денег на счёте с которого переводят,
+     * должен вернуть false.
+     */
+    public boolean transferMoney (String srcPassport, String srcRequisite, String destPassport, String dstRequisite, double amount) {
+        return true;
+    }
+}
