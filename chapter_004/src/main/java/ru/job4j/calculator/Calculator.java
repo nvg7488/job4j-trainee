892887e1ac8cd5ec846b48a903ec7143362dc8e1
nvg7488 <govoruchin_nv@mail.ru>
2019-03-28 14:16:36 +0700

    trainee_004.1(Labmda).1(Функциональные итерфейсы).Calculator(анонимный класс new Operation)

---
+++ chapter_004/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,31 @@
+package ru.job4j.calculator;
+/**
+ * Класс для вычисления арифметических операций.
+ * @author Николай Говорухин (govoruchin_nv@mail.ru)
+ */
+public class Calculator {
+    public interface Operation {
+        double calc(int left, int right);
+    }
+
+    public void multiple(int start, int finish, int value, Operation op) {
+        for (int index = start; index != finish; index++) {
+            System.out.println(
+                    op.calc(value, index)
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.multiple(
+                0, 10, 2,
+                new Operation() {
+                    @Override
+                    public double calc(int left, int right) {
+                        return left * right;
+                    }
+                }
+        );
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,10 @@
+package ru.job4j.calculator;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * @author Николай Говорухин (govoruchin_nv@mail.ru)
+ */
+public class CalculatorTest {
+
+}
