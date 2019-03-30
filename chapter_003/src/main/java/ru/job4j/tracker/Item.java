91267b7c653aae34bc30ca8a1b9bd5dcfabc18c0
nvg7488 <govoruchin_nv@mail.ru>
2019-03-30 19:58:17 +0700

    trainee_003.2(Обобщения).Tracker.Item

---
+++ chapter_003/src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,39 @@
+package ru.job4j.tracker;
+/**
+ * @author Николай Говорухин (govoruchin_nv@mail.ru)
+ */
+public class Item {
+    private String id;
+    private String name;
+    private String description;
+    private long create;
+
+    Item(String name, String description, long create) {
+        this.name = name;
+        this.create = create;
+        this.description = description;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getId() {
+        return this.id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public long getCreate() {
+        return this.create;
+    }
+
+    public void printInfo() {
+        System.out.println(
+                String.format("Item ID: %s\nName: %s\nDescription: %s",
+                this.getId(), this.getName(), this.getDescription())
+        );
+    }
+}
